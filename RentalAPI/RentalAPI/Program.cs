5fc8b49ce6c0109296e653a7365b42ca0b308487
TestRohan70 <testmailrohang@gmail.com>
2026-08-06 01:12:56 +0530

    Add new origin to CORS policy for frontend access

--- RentalAPI/RentalAPI/Program.cs
+++ RentalAPI/RentalAPI/Program.cs
@@ -60,7 +60,7 @@ builder.Services.AddCors(options =>
 {
     options.AddPolicy("AllowFrontend", policy =>
     {
-        policy.WithOrigins("http://localhost:4200", "https://localhost:4200")
+        policy.WithOrigins("http://localhost:4200", "https://localhost:4200","https://rental-ui-ten.vercel.app")
               .AllowAnyHeader()
               .AllowAnyMethod();
     });
